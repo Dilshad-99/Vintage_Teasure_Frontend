5bd50dc5ce54406ac0e1dffe25892df9babdbf47
Dilshad Patel <dilshad2307j@gmail.com>
2026-03-20 11:15:14 +0530

    Remove backend from frontend repo

--- CoachingProjectAPI/connection1.js
+++
@@ -1,9 +0,0 @@
-// import mongoose from "mongoose";
-
-// const url ='mongodb://127.0.0.1:27017/DilshadDatabase';
-
-// mongoose.connect(url);
-
-// console.log("mongodb connected successfully");
-
-
